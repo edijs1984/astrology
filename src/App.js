b60113c2c0a38445eb372f386e6e6ad0ed23e5d2
Edijs <ziedins.edijs@gmail.com>
2023-11-14 11:01:20 -0400

    remove dark background

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App-header">
-        <Astrology />
-      </div>
+      {/* <div className="App-header"> */}
+      <Astrology />
+      {/* </div> */}
     </div>
   );
 }
