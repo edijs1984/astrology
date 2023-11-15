ec8b79213986b173b33c8f85ba3f8022d3321f62
Edijs <ziedins.edijs@gmail.com>
2023-11-15 06:09:33 -0400

    adding dropdowns

--- src/Astrology.js
+++ src/Astrology.js
@@ -11,7 +11,7 @@ const formStyle = {
 const inputStyle = {
   margin: 10,
   height: 40,
-  width: 60,
+  width: 100,
   background: "none",
   border: "none",
   borderBottom: "1px solid #6536c9",
@@ -38,18 +38,18 @@ export default function Astrology() {
   });
   const formData = {
     p_year: firstPerson.p_year,
-    p_hour: firstPerson.p_month,
-    p_day: firstPerson.p_day,
     p_month: firstPerson.p_hour,
-    p_min: firstPerson.p_min,
+    p_day: firstPerson.p_day,
+    p_hour: 1,
+    p_min: 10,
     p_lat: 56.8,
     p_lon: 24.6,
     p_tzone: 2.5,
     s_year: secondPerson.s_year,
     s_month: secondPerson.s_month,
     s_day: secondPerson.s_day,
-    s_hour: secondPerson.s_hour,
-    s_min: secondPerson.s_min,
+    s_hour: 15,
+    s_min: 25,
     s_lat: 56.8,
     s_lon: 24.6,
     s_tzone: 2.5,
@@ -121,55 +121,67 @@ export default function Astrology() {
       <div
         style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
       >
-        <input
-          type="number"
+        <select
           value={firstPerson.p_year}
           onChange={(e) => handlePChange("p_year", e.target.value)}
           required
-          placeholder="Year"
           style={inputStyle}
-          min={1920}
-          max={2005}
-        />
-        <input
-          type="number"
-          value={firstPerson.p_month}
+        >
+          <option value="" disabled selected>
+            Year
+          </option>
+          {Array.from(
+            { length: 2005 - 1920 + 1 },
+            (_, index) => 2005 - index
+          ).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+
+        <select
+          value={firstPerson.p_year} // Make sure this should be p_month if you are tracking the month
           onChange={(e) => handlePChange("p_month", e.target.value)}
-          placeholder="Month"
+          required
           style={inputStyle}
-          min={1}
-          max={12}
-        />
-        <input
-          type="number"
+        >
+          {[
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ].map((month, index) => (
+            <option key={index} value={index + 1}>
+              {month}
+            </option>
+          ))}
+        </select>
+
+        <select
           value={firstPerson.p_day}
           onChange={(e) => handlePChange("p_day", e.target.value)}
           required
-          placeholder="day"
           style={inputStyle}
-          min={1}
-          max={31}
-        />
-        <input
-          type="number"
-          value={firstPerson.p_hour}
-          onChange={(e) => handlePChange("p_hour", e.target.value)}
-          required
-          placeholder="Hour"
-          style={inputStyle}
-          min={0}
-          max={23}
-        />
-        <input
-          type="number"
-          value={firstPerson.p_min}
-          onChange={(e) => handlePChange("p_min", e.target.value)}
-          required
-          placeholder="Minute"
-          style={inputStyle}
-          min={0}
-          max={59}
-        />
+          placeholder="Day"
+        >
+          <option value="" disabled selected>
+            Day
+          </option>
+          {Array.from({ length: 31 }, (_, index) => 1 + index).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
       </div>
       <div style={{ fontFamily: "sans-serif", fontSize: 24, marginTop: 10 }}>
         Partners data
@@ -177,55 +189,66 @@ export default function Astrology() {
       <div
         style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
       >
-        <input
-          type="number"
+        <select
           value={secondPerson.s_year}
           onChange={(e) => handleSChange("s_year", e.target.value)}
           required
-          placeholder="Year"
           style={inputStyle}
-          min={1920}
-          max={2005}
-        />
-        <input
-          type="number"
-          value={secondPerson.s_month}
+        >
+          <option value="" disabled selected>
+            Year
+          </option>
+          {Array.from(
+            { length: 2005 - 1920 + 1 },
+            (_, index) => 2005 - index
+          ).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+        <select
+          value={secondPerson.s_year} // Make sure this should be p_month if you are tracking the month
           onChange={(e) => handleSChange("s_month", e.target.value)}
-          placeholder="Month"
+          required
           style={inputStyle}
-          min={1}
-          max={12}
-        />
-        <input
-          type="number"
+        >
+          {[
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ].map((month, index) => (
+            <option key={index} value={index + 1}>
+              {month}
+            </option>
+          ))}
+        </select>
+
+        <select
           value={secondPerson.s_day}
           onChange={(e) => handleSChange("s_day", e.target.value)}
           required
-          placeholder="day"
           style={inputStyle}
-          min={1}
-          max={31}
-        />
-        <input
-          type="number"
-          value={secondPerson.s_hour}
-          onChange={(e) => handleSChange("s_hour", e.target.value)}
-          required
-          placeholder="Hour"
-          style={inputStyle}
-          min={0}
-          max={23}
-        />
-        <input
-          type="number"
-          value={secondPerson.s_min}
-          onChange={(e) => handleSChange("s_min", e.target.value)}
-          required
-          placeholder="Minute"
-          style={inputStyle}
-          min={0}
-          max={59}
-        />
+          placeholder="Day"
+        >
+          <option value="" disabled selected>
+            Day
+          </option>
+          {Array.from({ length: 31 }, (_, index) => 1 + index).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
       </div>
       <button
         type="submit"
